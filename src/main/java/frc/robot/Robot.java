71486ddde6b05891181d9275ee7e1867ff6a661a
Sean Maizel <robosean00@gmail.com>
2024-01-26 19:23:56 -0500

    Actually added advantagekit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,51 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.constants.BuildConstants;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
 
   @Override
   public void robotInit() {
+    Logger.recordMetadata("RuntimeType", getRuntimeType().toString());
+		Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+		Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+		Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+		Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+		Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+		Logger.recordMetadata("GitDirty", switch (BuildConstants.DIRTY) {
+			case 0 -> "Clean";
+			case 1 -> "Dirty";
+			default -> "Unknown";
+		});
+
+    if (Robot.isReal()) {
+      setUseTiming(false); // run as fast as possible
+      String logPath = LogFileUtil.findReplayLog(); // pull replay log from AdvantageScope (or prompt the user)
+      Logger.setReplaySource(new WPILOGReader(logPath));
+      Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+    }
+    else {
+      Logger.addDataReceiver(new WPILOGWriter("/media/sda1/logs/")); // log to a usb stick
+      Logger.addDataReceiver(new NT4Publisher()); // publish data to NetworkTables
+    }
+
     m_robotContainer = new RobotContainer();
+
+    Logger.start();
   }
 
   
